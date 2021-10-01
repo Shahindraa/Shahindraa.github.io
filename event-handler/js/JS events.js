c35e609c46c259fc882ca0e703980abc64f954cc
Shahindra Saravanan <shahindra981212@gmail.com>
2021-10-01 20:49:54 +0800

    test

--- event-handler/js/JS events.js
+++ event-handler/js/JS events.js
@@ -6,3 +6,17 @@ let elNamaPengguna = document.getElementById("namaPengguna")
 elNamaPengguna.addEventListener("keyup",function(){
     elNamaPengguna.value = transformUpperCase(elNamaPengguna.value)
 })
+
+function checkPassword(){
+    let elKataLaluan = document.getElementById("kataLaluan")
+    let pass_val = elKataLaluan.value
+
+    if(pass_val.length > 5){
+        alert ("Password check... OK!")
+    } else {
+        alert("Password is too short!")
+    }
+}
+
+let elKL = document.getElementById("kataLaluan")
+elKL.onblur = checkPassword
