a2c0dee185c2f57babf26adb0ac4ed9d5cd16091
Shahindra Saravanan <shahindra981212@gmail.com>
2021-10-08 21:56:52 +0800

    2156

--- fetch/js/fetch.js
+++ fetch/js/fetch.js
@@ -3,9 +3,9 @@ function LoadUser(){
 
     fetch(url)
     .then((response) => response.json())
-    .then (dat => {
-        let fn = document.getElementById("firstname")
-        let ln = document.getElementById("lastname")
+    .then (data => {
+        let fn = document.getElementById("firstName")
+        let ln = document.getElementById("lastName")
         let phone = document.getElementById("phone")
         let img = document.getElementById("userImg")
 
@@ -16,7 +16,76 @@ function LoadUser(){
     })
 }
 
-let eLoadUser = document.getElementById("getRandomUser")
-eLoadUser.addEventListener("click", function(){
+let elLoadUser = document.getElementById("getRandomUser")
+elLoadUser.addEventListener("click", function(){
     LoadUser()
 })
+
+let elLoadMultipleUsers = document.getElementById("getMultipleUser")
+elLoadMultipleUsers.addEventListener("click", function(){
+    let userCount = document.getElementById("userCount")
+    LoadMultipleUsers(userCount.value)
+})
+
+function LoadMultipleUsers(userCount){
+    let url ="https://randomuser.me/api/?results=" + userCount
+    let temp = ""
+
+    fetch(url)
+    .then((response) => response.json())
+    .then(shahi => {
+        let allUsers = document.getElementById("allUsers")
+
+        for(let i = 0; i < shahi.results.length; i++){
+            let fn = '<div>' + shahi.results[i].name.first +'</div>'
+            let ln = '<div>' + shahi.results[i].name.last +'</div>'
+            let phone = '<div>' + shahi.results[i].phone +'</div>'
+            let img = '<img src ="' + shahi.results[i].picture.large +'">'
+
+            temp = temp + fn + ln + phone + img 
+        }
+        allUsers.innerHTML = temp 
+    })
+} 
+
+let elLoadGender = document.getElementById("getGenderUser")
+elLoadGender.addEventListener("click",function(){
+    console.log("Load Gender clicked!")
+    // get the gender 
+    let gender = document.getElementById("userGender")
+    console.log("Selected Gender: " + gender.value)
+
+    // get the number of result
+    let count = document.getElementById("userGenderCount")
+    console.log("Count: " + count.value)
+
+    //call loadGender() adn pass gender and number of result
+    loadGender(gender.value, count.value)
+})
+
+function loadGender(gender, userCount){
+    let url ="https://randomuser.me/api/?results=" + userCount + "&gender=" + gender
+    console.log(url)
+
+    // call, fetch, loop the result and change the innerHTML for allGenderUser
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        let resultDiv = document.getElementById("allGenderUsers")
+        let temp = ""
+
+        for(let i = 0; i < data.results.length; i++){
+            let fn = '<div>' + data.results[i].name.first + '</div>'
+            let ln = '<div>' + data.results[i].name.last + '</div>'
+            let phone = '<div>' + data.results[i].phone + '</div>'
+            let img = '<img src="' + data.results[i].picture.large + '">'
+            let gdr = '<div>' + data.results[i].gender + '</div>'
+
+            temp = temp + fn + ln + phone + gdr + img
+        }
+        
+        resultDiv.innerHTML = temp
+
+    })
+}
+
